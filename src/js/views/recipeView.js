b3bf2b7e3049ab1e117f4589546d0e2db78a872c
joaomartins <joao82@gmail.com>
2023-05-14 15:16:53 +0200

    commit

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -106,7 +106,9 @@ class RecipeView extends View {
                      <use href="${icons}#icon-check"></use>
                      </svg>
                      <div class="recipe__quantity">${
-                       ing.quantity ? new Fraction(ing.quantity).toString() : ''
+                       ing.quantity
+                         ? Math.fraction(ing.quantity).toString()
+                         : ''
                      }</div>
                      <div class="recipe__description">
                      <span class="recipe__unit">${ing.unit}</span>
